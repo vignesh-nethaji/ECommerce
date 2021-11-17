71a51f12e804f43bd095b7b1326aac7f23989f10
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-11-17 19:46:59 +0530

    Init Api

---
+++ Api/ECommerce.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ECommerce.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ Api/ECommerce.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ECommerce.Services
+{
+    public class Class1
+    {
+    }
+}

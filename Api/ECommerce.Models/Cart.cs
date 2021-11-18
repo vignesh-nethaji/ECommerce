66560fdaac23cdad4cbc417ca185c482c1f5c73d
umadevi.m <umamagesh2210@gmail.com>
2021-11-18 17:02:05 +0530

    Added Cart Model

---
+++ Api/ECommerce.Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Quantity { get; set; }
+
+    }
+}

88b258aa957b168780f5e09f3a1881a5c8726ce9
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-11-18 18:50:57 +0530

    Implemented Dispose

--- Api/ECommerce.Services/Implementation/UserService.cs
+++ Api/ECommerce.Services/Implementation/UserService.cs
@@ -6,12 +6,17 @@ using System.Text;
 
 namespace ECommerce.Services.Implementation
 {
-    public class UserService : IUserService
+    public class UserService : IUserService, IDisposable
     {
         private readonly IUserRepository _userRepository;
         public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
+        }
+
+        public void Dispose()
+        {
+            _userRepository.Dispose();
         }
     }
 }

26b2829902f4d09231fb9b92a664187a1cc7e230
dinesh.b <ddinesh0125@gmail.com>
2021-11-20 19:44:43 +0530

    removed JSONIgnore

--- Api/ECommerce.Models/User.cs
+++ Api/ECommerce.Models/User.cs
@@ -8,7 +8,6 @@ namespace ECommerce.Models
         public int Id { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
-        [JsonIgnore]
         public string Password { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }

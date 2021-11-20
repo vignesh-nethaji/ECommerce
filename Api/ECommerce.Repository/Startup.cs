d874925c6dbc2bf2e9a59f91f213e9f93d3144c6
dinesh.b <ddinesh0125@gmail.com>
2021-11-20 19:42:46 +0530

    Added region

--- Api/ECommerce.Repository/Startup.cs
+++ Api/ECommerce.Repository/Startup.cs
@@ -16,10 +16,12 @@ namespace ECommerce.Repository
                     .AddDbContext<ECommerceContext>(options =>
                                                     options.UseSqlServer("server=localhost;Database=ECommerce;Trusted_Connection=True;"));
 
+            #region Repository
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IProductRepository, ProductRepository>();
             services.AddTransient<ICartRepository, CartRepository>();
+            #endregion
             return services;
         }
     }

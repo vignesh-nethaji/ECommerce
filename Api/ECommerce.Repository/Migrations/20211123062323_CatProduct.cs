ad26ea7ed8048b95eaf2f22abded0de836781576
deepak.sp <deepakrkm143@gmail.com>
2021-11-23 12:02:46 +0530

    Table relation

---
+++ Api/ECommerce.Repository/Migrations/20211123062323_CatProduct.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.Repository.Migrations
+{
+    public partial class CatProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Catagory_CategoryId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Catagory_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Catagory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Catagory_CategoryId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Product",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Catagory_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Catagory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

4b7c8a82a1d2aa408d4296f03e8e98c8a6ca7a8d
deepak.sp <deepakrkm143@gmail.com>
2021-11-23 12:01:19 +0530

    Change table relation in category and Product

--- Api/ECommerce.Models/Category.cs
+++ Api/ECommerce.Models/Category.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ECommerce.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-
+        [ForeignKey("CategoryId")]
         public virtual ICollection<Product> Product { get; set; }
     }
 }

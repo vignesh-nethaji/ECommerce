26f6147fdef378c2d5cd1918f8cb283bba18874b
karthick.s <karthick.s@utthunga.com>
2021-12-14 20:00:20 +0530

    CartPage responesive adjust.

--- app/src/ZeroDotOne/CartDetailsPage.js
+++ app/src/ZeroDotOne/CartDetailsPage.js
@@ -64,7 +64,7 @@ const CartDetailsPage = () => {
     }
 
     return (
-        <Container>
+        <div>
             <div>
                 <HeaderPage />
             </div>
@@ -117,16 +117,12 @@ const CartDetailsPage = () => {
                         <hr />
                         <Row>
                             <Col md="6">Price ({productDetails.length} items)</Col>
-                            <Col md="6">$ {total +(productDetails.length*199)}</Col>
-                            <Col md="6" sm="6" lg="6">Price ({productDetails.length} items)</Col>
-                            <Col md="6" sm="6" lg="6">$ {total + 199}</Col>
+                            <Col md="6">$ {total + (productDetails.length * 199)}</Col>
                         </Row >
                         <br />
                         <Row>
                             <Col md="6">Discount</Col>
-                            <Col md="6">$ {(productDetails.length*199)}</Col>
-                            <Col md="6" sm="6" lg="6">Discount</Col>
-                            <Col md="6" sm="6" lg="6">$ 199</Col>
+                            <Col md="6">$ {(productDetails.length * 199)}</Col>
                         </Row>
                         <br />
                         <Row>
@@ -146,14 +142,12 @@ const CartDetailsPage = () => {
                         <br />
                         <Row className="dotted">
                             <Col md="6">Total Amount</Col>
-                            <Col md="6">{total+100}</Col>
-                            <Col md="6" sm="6" lg="6">Total Amount</Col>
-                            <Col md="6" sm="6" lg="6">{total}</Col>
+                            <Col md="6">{total + 100}</Col>
                         </Row>
                     </CardHeader>
                 </Col>
             </Row>
-        </Container>
+        </div >
     )
 }
 
